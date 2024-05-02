9a40ce73218dd0a983e42815fcbb8588e0dd73d9
Jesus <143859436+Jesus-A-Flores@users.noreply.github.com>
2024-05-02 00:14:36 -0400

    Update exercice-1.py

--- exercices-level-1/exercice-1.py
+++ exercices-level-1/exercice-1.py
@@ -1,3 +1,8 @@
 '''Definir una función max() que tome como argumento dos números y devuelva el mayor de ellos.
 Es cierto que python tiene una función max() incorporada, pero hacerla nosotros mismos es un
 muy buen ejercicio'''
+def max(a,b):
+  if a > b:
+    return a
+  else:
+    return b
